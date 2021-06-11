a415204d7e39c769581514cb3fa40577e8bdf966
FILIPE PETTER <fgpetter@gmail.com>
2021-06-11 17:40:19 -0300

    Arquivos para inserção da base de dados

---
+++ ArquivosSQL/Clientes.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `customerx`.`clientes` (
+	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`nome` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+	`email` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+	`telefone` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+	`data_registro` DATETIME NOT NULL DEFAULT current_timestamp(),
+	PRIMARY KEY (`id`) USING BTREE
+)
+COLLATE 'utf8_general_ci';
+
+insert into clientes (nome, email, telefone) values ('Clerissa', 'cbiermatowicz0@reddit.com', '3817855006');
+insert into clientes (nome, email, telefone) values ('Thedrick', 'tsturror1@cloudflare.com', '8607949138');
+insert into clientes (nome, email, telefone) values ('Corabella', 'cwharton2@myspace.com', '6255525046');
+insert into clientes (nome, email, telefone) values ('Oberon', 'ocalder3@hud.gov', '9693568338');
+insert into clientes (nome, email, telefone) values ('Bartel', 'bhaycox4@nymag.com', '3807518083');
+insert into clientes (nome, email, telefone) values ('Jacqui', 'jbremond5@mtv.com', '1466610162');
+insert into clientes (nome, email, telefone) values ('Christiane', 'cdoidge6@google.fr', '5442475330');
+insert into clientes (nome, email, telefone) values ('Shaine', 'ssimonsen7@flickr.com', '2442351825');
+insert into clientes (nome, email, telefone) values ('Teri', 'thannaway8@bbc.co.uk', '4129346219');
+insert into clientes (nome, email, telefone) values ('Fields', 'fkirkman9@census.gov', '5154257816');
---
+++ ArquivosSQL/Usuarios.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `customerx`.`usuarios_copy` (
+	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`nome` VARCHAR(50) NOT NULL COLLATE 'utf8_general_ci',
+	`email` VARCHAR(50) NOT NULL COLLATE 'utf8_general_ci',
+	`senha` VARCHAR(50) NOT NULL COLLATE 'utf8_general_ci',
+	PRIMARY KEY (`id`) USING BTREE
+)
+ COLLATE 'utf8_general_ci';
+
+insert into usuarios (nome, email, senha) values ('admin', 'admin@admin.com', '21232f297a57a5a743894a0e4a801fc3');
